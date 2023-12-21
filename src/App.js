fdd773a9df1470c06afca570757f01642cd6dca4
Jigar-gokani <jigar.gokani@somaiya.edu>
2023-12-22 02:21:21 +0530

    project section integration

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import { ThemeProvider } from "styled-components";
+import { useState, useEffect } from "react";
+import { darkTheme, lightTheme } from './utils/Themes.js'
+import Navbar from "./components/Navbar";
 import './App.css';
-import styled, { ThemeProvider } from "styled-components";
-import { darkTheme } from './utils/Themes';
-import Navbar from './components/Navbar';
-import Hero from './components/HeroSection';
-import Skills from './components/Skills';
-import Education from './components/Education';
-import Experience from './components/Experience';
-
 import { BrowserRouter as Router } from 'react-router-dom';
-
-
+import HeroSection from "./components/HeroSection";
+// import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects"
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Experience from "./components/Experience";
+import Education from "./components/Education";
+import ProjectDetails from "./components/ProjectDetails";
+import styled from "styled-components";
 
 const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
@@ -22,20 +26,29 @@ const Wrapper = styled.div`
   width: 100%;
   clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
 `
-
 function App() {
+  const [openModal, setOpenModal] = useState({ state: false, project: null });
+  console.log(openModal)
   return (
     <ThemeProvider theme={darkTheme}>
-      < Router>
-      <Navbar/>
-      <Body>
-        <Hero/>
-        <Wrapper>
-         <Skills/>
-         <Experience/>
-         <Education/>
-        </Wrapper>
-      </Body>
+      <Router >
+        <Navbar />
+        <Body>
+          <HeroSection />
+          <Wrapper>
+            <Skills />
+            <Experience />
+          </Wrapper>
+          <Projects openModal={openModal} setOpenModal={setOpenModal} />
+          <Wrapper>
+            <Education />
+            <Contact />
+          </Wrapper>
+          <Footer />
+          {openModal.state &&
+            <ProjectDetails openModal={openModal} setOpenModal={setOpenModal} />
+          }
+        </Body>
       </Router>
     </ThemeProvider>
   );
